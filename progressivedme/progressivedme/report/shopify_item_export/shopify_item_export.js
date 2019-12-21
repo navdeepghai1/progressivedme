d51907d01c29fefb96a891681adcc9afd87b0c93
navdeepghai1 <navdeepghai1@gmail.com>
2019-12-21 22:26:30 +0400

    feat(Report): fixes the report with shopify format prices(variant price)

--- progressivedme/progressivedme/report/shopify_item_export/shopify_item_export.js
+++ progressivedme/progressivedme/report/shopify_item_export/shopify_item_export.js
@@ -6,12 +6,46 @@ frappe.query_reports["Shopify Item Export"] = {
 	"filters": [{
 			"fieldname": "show_items_with_image",
 			"fieldtype": "Check",
-			"label": "Show Items With Image",
+			"label": __("Show Items With Image"),
 			"default": true,
 		},{
 			"fieldname": "item_group",
 			"fieldtype": "Link",
 			"options": "Item Group",
-			"label": "Item Category",
+			"label": __("Item Category(Major Category)"),
+			"get_query": function(){
+				return{
+					"filters":{
+						"is_group": 1
+					}
+				}
+			}
+		},{
+			"fieldname": "sub_item_group",
+			"fieldtype": "Link",
+			"options": "Item Group",
+			"label": __("Sub Item Group(Minor Category)"),
+			"get_query": function(){
+				let parent_item_group = cur_page.page.page.fields_dict.item_group.get_value();
+				let filters = {
+					"is_group": 0
+				};
+				if(parent_item_group){
+					$.extend(filters, {
+						"parent_item_group": parent_item_group
+					});
+					return {
+						"filters": filters
+					}
+				}
+			}
+		},{
+			"fieldname": "start",
+			"fieldtype": "Int",
+			"label": __("Start"),
+		},{
+			"fieldname": "end",
+			"fieldtype": "Int",
+			"label": __("End"),
 	}]
 }
--- progressivedme/progressivedme/report/shopify_item_export/shopify_item_export.py
+++ progressivedme/progressivedme/report/shopify_item_export/shopify_item_export.py
@@ -3,7 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe import scrub
+from frappe import scrub, _
 from frappe.utils import flt, cint, cstr
 import math
 
@@ -12,144 +12,154 @@ def execute(filters=None):
 
 class ShopifyItemExport(object):
         
-        def __init__(self, filters):
-                self.filters = frappe._dict(filters or {})
+	def __init__(self, filters):
+		self.filters = frappe._dict(filters or {})
 
-        def run(self):
-                columns = self.get_columns()
-                data = []
-                items = self.get_items()
-                for key in sorted(items.keys()):
-                        item = items[key]
-                        item_price = (math.trunc(item.list_price)-1) + 0.99
-                        data.append({
-                                "handle": item.name,
-                                "title": item.item_name,
-                                "body_html": item.extended_description,
-                                "vendor": item.vendor,
-                                "type": item.item_group,
-                                "variant_price": item_price,
-                                "list_price": item.list_price,
-                                "variant_taxable": "",
-                                "standard_price": item.standard_price,
-                                "option1_value": "%s - Height: %s x Width: %s "%(item.item_name, item.product_height, item.product_width), 
-                                "image_src": "https://navdeepghai1.info%s"%(item.image) if item.image else "",
-                                "image_position": 1,
-                                "image_alt_text": item.item_name,
-                                "seo_title": item.item_name,
-                                "seo_description": item.extended_description,
-                        })
+	def run(self):
+		columns = self.get_columns()
+		data = []
+		items = self.get_items()
+		for key in sorted(items.keys()):
+			item = items[key]
+			item_price = (math.trunc(item.list_price)-1) + 0.99
+			data.append({
+				"handle": item.name,
+				"title": item.item_name,
+				"body_html": item.extended_description if item.extended_description != "nan" else item.item_name,
+				"vendor": item.vendor,
+				"type": item.item_group,
+				"variant_price": item_price,
+				"list_price": item.list_price,
+				"variant_taxable": "",
+				"tags":" ".join([item.sub_item_group]),
+				"standard_price": item.standard_price,
+				"option1_value": "%s - Height: %s x Width: %s "%(item.item_name, item.product_height, item.product_width), 
+				"image_src": "https://navdeepghai1.info%s"%(item.image) if item.image else "",
+				"image_position": 1,
+				"image_alt_text": item.item_name,
+				"seo_title": item.item_name,
+				"seo_description": item.extended_description,
+				"published": item.published
+			})
 
-                return columns, data
+		return columns, data
 
-        def get_items(self):
-                conditions = ""
-                if self.filters.get("show_items_with_image"):
-                        conditions += " AND `tabItem`.image IS NOT NULL "
-                else:
-                        conditions += " AND `tabItem`.image IS NULL "
-                if self.filters.item_group:
-                        conditions += " AND `tabItem`.item_group = '%s' "%(self.filters.item_group)
-                items = frappe._dict()
-                for item in frappe.db.sql(""" SELECT `tabItem`.name, `tabItem`.item_group,
-                            `tabItem`.item_name, `tabItem`.description, `tabItem`.extended_description,
-                            `tabItem`.vendor_number as vendor, `tabItem`.image
-                            FROM
-                                    `tabItem`
-                            WHERE
-                                    `tabItem`.disabled = 0 %s
-                            ORDER BY
-                                    `tabItem`.item_name ASC, `tabItem`.item_group
-                            """%(conditions), as_dict=True):
-                        items.setdefault(item.name, item)
+	def get_items(self):
+		conditions = ""
+		limits = ""
+		if self.filters.get("start") or self.filters.get("end"):
+			limits += " LIMIT "
+			if self.filters.get("start"):
+				limits += " %s"%(self.filters.get("start"))
+			if self.filters.get("start") and self.filters.get("end"):
+				limits += ",%s"%(self.filters.get("end"))
 
-                self.update_price_list_for_items("Standard Price", items)
-                self.update_price_list_for_items("List Price", items)
-                return items
+		if self.filters.get("show_items_with_image"):
+			conditions += " AND `tabItem`.image IS NOT NULL "
+		else:
+			conditions += " AND `tabItem`.image IS NULL "
+		if self.filters.sub_item_group:
+			conditions += " AND `tabItem`.item_group = '%s' "%(self.filters.sub_item_group)
+		if self.filters.item_group and not self.filters.sub_item_group:
+			conditions += """ AND `tabItem`.item_group IN (SELECT `tabItem Group`.name FROM `tabItem Group`
+					WHERE `tabItem Group`.parent_item_group = '%s')"""%(self.filters.item_group)
+		items = frappe._dict()
+		for item in frappe.db.sql(""" SELECT `tabItem`.name,
+				`tabItem`.item_name, `tabItem`.description, `tabItem`.extended_description,
+				`tabItem`.vendor_number as vendor, `tabItem`.image,
+				`tabItem Group`.parent_item_group AS item_group,
+				`tabItem Group`.name as sub_item_group,
+				'TRUE' AS published,
+				(SELECT `tabItem Price`.price_list_rate FROM `tabItem Price`
+					WHERE `tabItem Price`.item_code = `tabItem`.name AND
+						`tabItem Price`.price_list = '%s' LIMIT 1) AS list_price,
+				(SELECT `tabItem Price`.price_list_rate FROM `tabItem Price`
+					WHERE `tabItem Price`.item_code = `tabItem`.name AND
+						`tabItem Price`.price_list = '%s' LIMIT 1) AS standard_price
+				FROM
+					`tabItem` INNER JOIN `tabItem Group`
+				ON
+					`tabItem`.item_group = `tabItem Group`.name
+				WHERE
+					`tabItem`.disabled = 0 %s
+				ORDER BY
+					`tabItem`.name, `tabItem Group`.parent_item_group %s
+				"""%('List Price', 'Standard Price', conditions, limits), as_dict=True):
+			items.setdefault(item.name, item)
 
-        def update_price_list_for_items(self, price_list, items):
-                fieldname = scrub(price_list)
-                for item_price in frappe.db.sql(""" SELECT `tabItem Price`.price_list_rate,
-                            `tabItem Price`.item_code
-                            FROM `tabItem Price` WHERE price_list = '%s' """%(price_list), as_dict=True):
-                        if item_price.item_code in items:
-                                items[item_price.item_code][fieldname] = item_price.get("price_list_rate")
+		return items
 
-        def get_columns(self):
-                return [{
-                            "fieldname": "handle",
-                            "label": "Handle",
-                            "fieldtype": "Data",
-                            "width": 200,
-                        },{
-                            "fieldname": "title",
-                            "label": "Title",
-                            "fieldtype": "Data",
-                            "width": 200,
-                        },{
-                            "fieldname": "body_html",
-                            "fieldtype": "Data",
-                            "label": "Body (HTML)",
-                            "width": 200,
-                        },{
-                            "fieldname": "vendor",
-                            "fieldtype": "Data",
-                            "label": "Vendor",
-                            "width": 100
-                        },{
-                            "fieldtype": "Data",
-                            "fieldname": "type",
-                            "label": "Type",
-                            "width": 150,
-                        },{
-                            "fieldname": "variant_price",
-                            "fieldtype": "Float",
-                            "label": "Variant Price",
-                            "width": 120,
-                        },{
-                            "fieldname": "list_price",
-                            "fieldtype": "Float",
-                            "label": "List Price",
-                            "width": 120,
-                        },{
-                            "fieldname": "standard_price",
-                            "fieldtype": "Float",
-                            "label": "Standard Price",
-                            "width": 100,
-                        },{
-                            "fieldname": "option1_value",
-                            "fieldtype": "Data",
-                            "label": "Option1 Value",
-                            "width": 120,
+	def get_columns(self):
+		return [{
+				"fieldname": "handle",
+				"label": "Handle",
+				"fieldtype": "Data",
+				"width": 200,
+			},{
+				"fieldname": "title",
+				"label": "Title",
+				"fieldtype": "Data",
+				"width": 200,
+			},{
+				"fieldname": "body_html",
+				"fieldtype": "Data",
+				"label": "Body (HTML)",
+				"width": 200,
+			},{
+				"fieldname": "vendor",
+				"fieldtype": "Data",
+				"label": "Vendor",
+				"width": 100
+			},{
+				"fieldtype": "Data",
+				"fieldname": "type",
+				"label": "Type",
+				"width": 150,
+			},{
+				"fieldname": "tags",
+				"fieldtype": "Data",
+				"label": _("Tags"),
+				"width": 150,
+			},{
+				"fieldname": "variant_price",
+				"fieldtype": "Float",
+				"label": "Variant Price",
+				"width": 120,
+			},{
+				"fieldname": "option1_value",
+				"fieldtype": "Data",
+				"label": "Option1 Value",
+				"width": 120,
 
-                        },{
-                            "fieldname": "image_src",
-                            "fieldtype": "Data",
-                            "label": "Image Src",
-                            "width": 200,
-                        },{
-                            "fieldname": "variant_taxable",
-                            "fieldtype": "Data",
-                            "label": "Variant Taxable",
-                            "width": 80
-                        },{
-                            "fieldname": "image_position",
-                            "fieldtype": "Int",
-                            "label": "Image Position",
-                            "width": 70,
-                        },{
-                            "fieldname": "image_alt_text",
-                            "fieldtype": "Data",
-                            "label": "Image Alt Text",
-                            "width": 200,
-                        },{
-                            "fieldname": "seo_title",
-                            "fieldtype": "Data",
-                            "label": "SEO Title",
-                            "width": 200,
-                        },{
-                            "fieldname": "seo_description",
-                            "fieldtype": "Data",
-                            "label": "SEO Description",
-                            "width": 200
-                }]
+			},{
+				"fieldname": "image_src",
+				"fieldtype": "Data",
+				"label": "Image Src",
+				"width": 200,
+			},{
+				"fieldname": "variant_taxable",
+				"fieldtype": "Data",
+				"label": "Variant Taxable",
+				"width": 80
+			},{
+				"fieldname": "image_position",
+				"fieldtype": "Int",
+				"label": "Image Position",
+				"width": 70,
+			},{
+				"fieldname": "image_alt_text",
+				"fieldtype": "Data",
+				"label": "Image Alt Text",
+				"width": 200,
+			},{
+				"fieldname": "seo_title",
+				"fieldtype": "Data",
+				"label": "SEO Title",
+				"width": 200,
+			},{
+				"fieldname": "seo_description",
+				"fieldtype": "Data",
+				"label": "SEO Description",
+				"width": 200,
+
+		}]
